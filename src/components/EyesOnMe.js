eb37c41681ea0dc10e12b2d2c14b3a0a2bcc4ed4
Muhammed <muhammadburhan964@gmail.com>
2021-07-27 17:21:09 +0300

    commit tree 7debc4cceee11360ffcd10b724da26af83156be4

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  const foucs = () => {
+    console.log("Good!");
+  };
+  const blur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+  return (
+    <div>
+      <button
+        onFocus={() => {
+          foucs();
+        }}
+        onBlur={() => {
+          blur();
+        }}
+      >
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+  const changeHandler = (e) => {
+    console.log("Entering password...");
+  };
+  return (
+    <div>
+      <input
+        onChange={() => {
+          changeHandler();
+        }}
+        type="password"
+        name=""
+        id=""
+      />
+    </div>
+  );
+}
+
+export default Keypad;
